4280d7f636a59e7a0349532b95e4ea7cc6634b92
AtilioA <atiliogpg@protonmail.com>
2022-03-10 15:25:52 -0300

    Draw `Terrain` in 3D by using `z` dimension
    Resolves #12

--- src/terrain.cpp
+++ src/terrain.cpp
@@ -12,12 +12,45 @@ void Terrain::draw_terrain()
 
     glTranslatef(this->center.x, -this->center.y, 0);
 
-    // Draw terrain from the top left corner to the bottom right corner
-    glBegin(GL_POLYGON);
-    glVertex2f(0, 0);
-    glVertex2f(this->width, 0);
-    glVertex2f(this->width, -this->height);
-    glVertex2f(0, -this->height);
+    // Draw each face from the top left corner to the bottom right corner using GL_QUADS. Use the Terrain width, height, and length to determine the dimensions of each of the six faces.
+    glBegin(GL_QUADS);
+
+    // FRONT FACE
+    glVertex3f(0, 0, 0);
+    glVertex3f(this->width, 0, 0);
+    glVertex3f(this->width, -this->height, 0);
+    glVertex3f(0, -this->height, 0);
+
+    // BACK FACE
+    glVertex3f(0, 0, -this->length);
+    glVertex3f(0, -this->height, -this->length);
+    glVertex3f(this->width, 0, -this->length);
+    glVertex3f(this->width, -this->height, -this->length);
+
+    // TOP FACE
+    glVertex3f(0, 0, 0);
+    glVertex3f(this->width, 0, 0);
+    glVertex3f(this->width, 0, -this->length);
+    glVertex3f(0, 0, -this->length);
+
+    // BOTTOM FACE
+    glVertex3f(0, 0, -this->length);
+    glVertex3f(this->width, 0, -this->length);
+    glVertex3f(this->width, -this->height, -this->length);
+    glVertex3f(0, -this->height, -this->length);
+
+    // LEFT FACE
+    glVertex3f(0, 0, 0);
+    glVertex3f(0, 0, -this->length);
+    glVertex3f(0, -this->height, -this->length);
+    glVertex3f(0, -this->height, 0);
+
+    // RIGHT FACE
+    glVertex3f(this->width, 0, 0);
+    glVertex3f(this->width, 0, -this->length);
+    glVertex3f(this->width, -this->height, -this->length);
+    glVertex3f(this->width, -this->height, 0);
+
     glEnd();
     glPopMatrix();
 
